fb1dda62f68b4709338ebb8da498c6353768198e
Dhinesh T <dhinesh@dorustree.in>
2023-05-16 19:05:51 +0530

    final  commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     <div className="flex justify-between items-center h-24 max-[w-1240px] mx-auto px-6 text-white">
       <h1 className="w-full text-3xl text-[#00df9a] font-semibold">React.</h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
+        <li className="p-4">Home </li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
